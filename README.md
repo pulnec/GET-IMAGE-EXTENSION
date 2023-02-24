3ec1d3f94b24e6e6c944a339060486848ef6890e
Jorge Andrade <andrade091.app@gmail.com>
2023-02-23 21:57:41 -0300

    new methos

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 # Get URL Image
 
+Option 1: "Get Random Image"
+
 Step 1: use the command inline code when you need add a url image
 
-Step 2: the command @image:name_image , example. "@image:cat" , "@image:department"
+Step 2: the command @img:name_image , example. "@img:cat" , "@img:department"
 
 Step 3: select text only command.
 
 Step 4: open command list with command + shift + P , find "Request Image"
+
+Option 2: "Open Search Image"
+
+Step 1: write in the search input
+
+Step 2: Show preview images
+
+Step 3: Select the image
 
 ## Demo
 
--- extension.js
+++ extension.js
@@ -17,6 +17,52 @@ function activate(context) {
     "&q=" +
     encodeURIComponent(value);
 
+  let disposableTwo = vscode.commands.registerCommand(
+    "imagegenerator.randomImageRequest",
+    async function () {
+      // Get the active text editor
+      const editor = vscode.window.activeTextEditor;
+
+      if (editor) {
+        const document = editor.document;
+        const selection = editor.selection;
+        // Get the word within the selection
+        const word = document.getText(selection);
+        //validation
+        try {
+          if (word.includes("@img:")) {
+            const part = word.split(":");
+            if (part.length === 2) {
+              const queryImage = part[1];
+              const response = await axios.get(URL(queryImage));
+              if (!response.data.hits.length) {
+                vscode.window.showInformationMessage(
+                  "Sorry, I did not find a picture for you"
+                );
+                return false;
+              }
+
+              const { largeImageURL } = response.data.hits[0];
+              editor.edit((editBuilder) => {
+                editBuilder.replace(selection, largeImageURL);
+              });
+              vscode.window.showInformationMessage("URL Image apply :) !");
+            } else {
+              throw new Error();
+            }
+          } else {
+            throw new Error();
+          }
+        } catch (e) {
+          vscode.window.showWarningMessage(
+            "Command format incorrect please, Ex. @img:find_image_name"
+          );
+        }
+        console.log("wordParse", word);
+      }
+    }
+  );
+
   let disposable = vscode.commands.registerCommand(
     "imagegenerator.startImageRequest",
     function () {
@@ -26,70 +72,115 @@ function activate(context) {
           const response = await axios.get(URL(value));
           if (!response.data.hits.length) {
             vscode.window.showInformationMessage(
-              "Hello World from ImageGenerator! " + value
+              "Sorry, I did not find a picture for you"
             );
             return false;
           }
 
           const panel = vscode.window.createWebviewPanel(
-            'imagegenerator',
-            'Lista de Imagenes',
+            "imagegenerator",
+            "Lista de Imagenes",
             vscode.ViewColumn.Two,
             getWebviewOptions(context.extensionUri)
           );
 
           panel.webview.onDidReceiveMessage(
-            message => {
+            async (message) => {
               switch (message.command) {
-                case 'alert':
-                  vscode.window.showErrorMessage(message.text)
-                case 'setUrl':
-                  vscode.env.clipboard.writeText(message.text)
-                  vscode.window.showInformationMessage('URL copiada en portapapeles')
-                  panel.dispose()
-                return;
+                case "alert":
+                  vscode.window.showErrorMessage(message.text);
+                case "setUrl":
+                  /* vscode.env.clipboard.writeText(message.text);
+                  vscode.window.showInformationMessage(
+                    "URL copiada en portapapeles"
+                  ); */
+
+                  const optionsProgress = {
+                    location: vscode.ProgressLocation.Notification,
+                    title: "Get Image",
+                    cancellable: false,
+                  };
+
+                  await vscode.window.withProgress(
+                    optionsProgress,
+                    (progress) => {
+                      return new Promise(async (resolve) => {
+                        progress.report({ message: "Extrayendo URL" });
+                        setTimeout(() => {
+                          const editor = vscode.window.activeTextEditor;
+                          if (editor) {
+                            editor.edit((editBuilder) => {
+                              editBuilder.insert(
+                                editor.selection.active,
+                                message.text
+                              );
+                            });
+                          }
+                        }, 800);
+                        panel.dispose();
+                        resolve();
+                      });
+                    }
+                  );
+                  return;
               }
             },
             null,
             []
           );
 
-          panel.webview.html = getHtmlForWebview(panel.webview,response.data.hits)
-
+          panel.webview.html = getHtmlForWebview(
+            panel.webview,
+            response.data.hits
+          );
         });
     }
   );
 
-  const getHtmlForWebview = (webview,list) => {
-    const extensionUri = context.extensionUri
-		// Local path to main script run in the webview
-		const scriptPathOnDisk = vscode.Uri.joinPath(extensionUri, './media', 'main.js');
+  const getHtmlForWebview = (webview, list) => {
+    const extensionUri = context.extensionUri;
+    // Local path to main script run in the webview
+    const scriptPathOnDisk = vscode.Uri.joinPath(
+      extensionUri,
+      "./media",
+      "main.js"
+    );
 
-		// And the uri we use to load this script in the webview
-		const scriptUri = webview.asWebviewUri(scriptPathOnDisk);
+    // And the uri we use to load this script in the webview
+    const scriptUri = webview.asWebviewUri(scriptPathOnDisk);
 
-		// Local path to css styles
-		const styleResetPath = vscode.Uri.joinPath(extensionUri, './media', 'reset.css');
-		const stylesPathMainPath = vscode.Uri.joinPath(extensionUri, './media', 'vscode.css');
+    // Local path to css styles
+    const styleResetPath = vscode.Uri.joinPath(
+      extensionUri,
+      "./media",
+      "reset.css"
+    );
+    const stylesPathMainPath = vscode.Uri.joinPath(
+      extensionUri,
+      "./media",
+      "vscode.css"
+    );
 
-		// Uri to load styles into webview
-		const stylesResetUri = webview.asWebviewUri(styleResetPath);
-		const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
+    // Uri to load styles into webview
+    const stylesResetUri = webview.asWebviewUri(styleResetPath);
+    const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
 
-		// Use a nonce to only allow specific scripts to be run
-		const nonce = getNonce();
-    const strImg = list.reduce((stringImg,img) => {
-      return `
+    // Use a nonce to only allow specific scripts to be run
+    const nonce = getNonce();
+    const strImg = list.reduce((stringImg, img) => {
+      return (
+        `
         <img 
           class="imageSelect"
           src="${img.previewURL}" 
           alt="${img.tags}" 
           data-id:"${img.id}" 
           width="200" height="200" 
-        /></br> `+stringImg
-    },"")
+        /></br> ` + stringImg
+      );
+    }, "");
 
-		return `<!DOCTYPE html>
+    return `<!DOCTYPE html>
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
@@ -110,29 +201,30 @@ function activate(context) {
 				<script nonce="${nonce}" src="${scriptUri}"></script>
 			</body>
 			</html>`;
-	}
+  };
 
-  context.subscriptions.push(disposable)
+  context.subscriptions.push(disposable, disposableTwo);
 }
 
 const getNonce = () => {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
-}
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
 
 const getWebviewOptions = (extensionUri) => {
-	return {
-		// Enable javascript in the webview
-		enableScripts: true,
+  return {
+    // Enable javascript in the webview
+    enableScripts: true,
 
-		// And restrict the webview to only loading content from our extension's `media` directory.
-		localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')]
-	};
-}
+    // And restrict the webview to only loading content from our extension's `media` directory.
+    localResourceRoots: [vscode.Uri.joinPath(extensionUri, "media")],
+  };
+};
 
 // This method is called when your extension is deactivated
 function deactivate() {}
